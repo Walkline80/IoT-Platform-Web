dd1ad09bf38359f5ebc914d85ea9afdf1440d58a
Walkline <walkline@163.com>
2019-06-21 22:23:59 +0800

    使用代码段优化首页php文件

--- index.php
+++ index.php
@@ -4,29 +4,9 @@
 	session_set_cookie_params(24 * 3600 * 365, "/");
 	session_start();
 
-	const sign_up_code =
-		'<nav class="navbar navbar-dark bg-dark">
-			<a class="navbar-brand mb-0 h1" href="/">走线物联</a>
-
-			<span class="navbar-text">还没注册？<a href="./signup.php">马上注册</a></span>
-		</nav>';
-	
-	const log_out_code =
-		'<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-			<a class="navbar-brand mb-0 h1" href="/">走线物联</a>
-			
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
-
-			<div class="collapse navbar-collapse" id="navbarNav">
-				<div class="navbar-nav mr-auto">
-					<a class="nav-item nav-link" href="console.php">我的控制台</a>
-					<a class="nav-item nav-link" href="#" id="logout">退出</a>
-				</div>
-			</div>
-		</nav>';
-		// <span class="navbar-text">Hello, World</span>
+	define("sign_up_code", file_get_contents("interface/snippet/index_sign_up.html"));	
+	define("log_out_code", file_get_contents("interface/snippet/index_log_out.html"));
+	// <span class="navbar-text">Hello, World</span>
 
 	$page = "index";
 
